169aca94acd1989d8964cf61a2dbbd6b4df7d5a9
Shareef Ali <64172657+shareef945@users.noreply.github.com>
2024-01-11 23:17:53 +0000

    Initial commit from Create Next App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 import { Inter as FontSans } from "next/font/google"
 
 
