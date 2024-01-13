4550af2ff8b7514ed272fa4327a398fb890bea63
Shareef Ali <64172657+shareef945@users.noreply.github.com>
2024-01-13 15:42:04 +0000

    wip

--- utils/utils.ts
+++ utils/utils.ts
@@ -23,3 +23,32 @@ export function formatDate(dateString: string): string {
   const formattedDate = `${dayWithSuffix} ${month} ${year}`;
   return formattedDate;
 }
+
+export function formatDateQuery(date: Date): string {
+  const pad = (num: number): string => num?.toString().padStart(2, "0");
+
+  const year = date?.getUTCFullYear();
+  const month = pad(date?.getUTCMonth() + 1); // getUTCMonth() returns 0-11
+  const day = pad(date?.getUTCDate());
+  const hours = pad(date?.getUTCHours());
+  const minutes = pad(date?.getUTCMinutes());
+  const seconds = pad(date?.getUTCSeconds());
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}+00`;
+}
+
+export const handleDownload = (data: any) => {
+  const csvContent = data
+    .map((row: any) => Object.values(row).join(","))
+    .join("\n");
+  const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = "transactions.csv";
+  document.body.appendChild(link);
+
+  link.click();
+
+  document.body.removeChild(link);
+};
