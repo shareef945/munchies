9bf9fa56cd0d0d02632453b44ed303bee07a5d2d
Shareef Ali <64172657+shareef945@users.noreply.github.com>
2024-08-07 14:43:43 +0000

    feature: authtoken from local storage

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,20 +1,37 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 import { ANALYTICS_BASE_URL, AUTH_BASE_URL } from "@/config/config";
 
-const createApi = (baseURL: string | undefined) => {
+const createApi = (baseURL: string | undefined): AxiosInstance => {
   if (!baseURL) {
     throw new Error(
       `Base URL is not defined. Please check your environment variables.`
     );
   }
 
-  return axios.create({
+  const instance = axios.create({
     baseURL,
     headers: {
       "Content-Type": "application/json",
     },
     withCredentials: true,
   });
+
+  instance.interceptors.request.use(
+    (config) => {
+      if (typeof window !== "undefined") {
+        const token = localStorage.getItem("authToken");
+        if (token) {
+          config.headers["Authorization"] = `Bearer ${token}`;
+        }
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  return instance;
 };
 
 export const authApi = createApi(AUTH_BASE_URL);
